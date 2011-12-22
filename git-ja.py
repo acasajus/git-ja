c0abd9baf02aafaee6efc96bd51c465ad22ad668
Adrià Casajús <adria@ecm.ub.es>
2011-12-22 16:36:15 +0100

    NEW: Added prunning magic

--- git-ja.py
+++ git-ja.py
@@ -11,10 +11,11 @@ parser.add_argument( '-d', '--debug', action = 'store_true', help = "Enable debu
 parser.add_argument( '-l', '--list', action = 'store_true', help = "List all remotes" )
 parser.add_argument( '-u', '--update', action = 'store_true', help = "Update all remotes" )
 parser.add_argument( '-t', '--status', action = 'store_true', help = "Show status of all branches with remotes" )
-parser.add_argument( '-i', '--divergence', action = 'store_true', help = "Show divergence tree of given commits" )
-parser.add_argument( '-a', '--aheadlog', action = 'store_true', help = "Retrieve a list of commits ahead of tracked remote branch" )
+parser.add_argument( '-i', '--divergence', action = 'store_true', help = "Show divergence tree of given refs" )
+parser.add_argument( '-a', '--aheadlog', action = 'store_true', help = "Retrieve a list of refs ahead of tracked remote branch" )
+parser.add_argument( '-P', '--prune', action = 'store_true', help = "Prune origin branches that no longer exist in the local repository" )
 parser.add_argument( '-s', '--shy', action = 'store_true', help = "Do not execute any remote query" )
-parser.add_argument( 'commits', metavar='commit', nargs='*' )
+parser.add_argument( 'refs', metavar='ref', nargs='*' )
 doneStuff = False
 parseRes = parser.parse_args()
 
@@ -31,27 +32,29 @@ def getBranchTacks( head ):
   merge = "/".join( do( "git config branch.%s.merge" % head ).split( "/" )[2:] )
   return "%s/%s" % ( remote, merge )
 
-def exists( commit ):
-  return do( "git log -n 1 --pretty=oneline %s" % commit, checkValid = True )
+def exists( ref ):
+  return do( "git log -n 1 --pretty=oneline %s" % ref, checkValid = True )
 
   
-def getCommits( defaultAll = True, insertTrackedIfOne = False ):
-  for commit in parseRes.commits:
-    if not exists( commit ):
-      print "Commit %s doesn't seem to be valid" % commit
+def getRefs( defaultAll = False, defaultCurrent = False, insertTrackedIfOne = False ):
+  for ref in parseRes.refs:
+    if not exists( ref ):
+      print "Ref %s doesn't seem to be valid" % ref
       sys.exit(1)
-  if parseRes.commits:
-    commits = parseRes.commits
+  if parseRes.refs:
+    refs = parseRes.refs
   elif defaultAll:
-    commits = [ com.lstrip( '*' ).strip() for com in do( "git br" ).split( "\n" ) if com.strip() ]
+    refs = [ com.lstrip( '*' ).strip() for com in do( "git br" ).split( "\n" ) if com.strip() ]
+  elif defaultCurrent:
+    refs = [ com.lstrip( '*' ).strip() for com in do( "git br" ).split( "\n" ) if com.strip().find( "*" ) == 0 ]
   else:
-    commits = [ com.lstrip( '*' ).strip() for com in do( "git br" ).split( "\n" ) if com.strip().find( "*" ) == 0 ]
-  if len( commits ) and insertTrackedIfOne:
-    commits.insert( 0, getBranchTacks( commits[0] ) )
-  return commits
+    refs = []
+  if len( refs ) and insertTrackedIfOne:
+    refs.insert( 0, getBranchTacks( refs[0] ) )
+  return refs
       
 
-def getCommit( head ):
+def getRef( head ):
   return do( "git log -n 1 --pretty=format:%%H %s" % head )
  
 def do( cmd, checkValid = False ):
@@ -80,8 +83,9 @@ def do( cmd, checkValid = False ):
   sp.stdout.close()
   return data.rstrip()
 
-if parseRes.list:
-  doneStuff = True
+#Actual stuff
+  
+def execList():
   remotes = magicSplit( do( "git remote show" ) )
   print "%s remotes: %s" % ( len( remotes ), ", ".join( remotes ) )
   for remote in remotes:
@@ -89,56 +93,111 @@ if parseRes.list:
     if parseRes.shy:
       cmd += " -n"
     print do( cmd )
-if parseRes.update:
-  doneStuff = True
+
+def execUpdate():
   print "Updating remotes..."
   print do( "git remote update --prune" )
   
-## Divergence
-if parseRes.divergence:
-  doneStuff = True
-  commits = getCommits( insertTrackedIfOne = True )
-  mergeBase = do( "git merge-base --octopus %s" % " ".join( commits ) )
-  if not commits:
-    commits = [ "HEAD" ]
+def execDivergence():
+  refs = getRefs( defaultAll = True, insertTrackedIfOne = True )
+  mergeBase = do( "git merge-base --octopus %s" % " ".join( refs ) )
+  if not refs:
+    refs = [ "HEAD" ]
   iP = 0
-  while iP < len( commits ):
-    if mergeBase == getCommit( commits[iP] ):
-      mergeBase = commits[iP]
-      commits.pop( iP )
-    iP += 1
-  if not commits:
-    print "There is no history betweed defined commits"
+  while iP < len( refs ):
+    if mergeBase == getRef( refs[iP] ):
+      mergeBase = refs[iP]
+      refs.pop( iP )
+      break
+  if not refs:
+    print "There is no history betweed defined refs"
   else:
-    cmd = "git log --graph --color --boundary --pretty=format:%s %s..%s" % ( "%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s", mergeBase, " ".join( commits ) )
+    cmd = "git log --graph --color --boundary --pretty=format:%s %s..%s" % ( "%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s", mergeBase, " ".join( refs ) )
     print do( cmd )
 
-## Commlog
-
-if parseRes.aheadlog:
-  doneStuff = True
+def execAheadLog():
   remlog = re.compile( "^ *\*? +([\-\w]+) +\w+ +\[(ahead|behind) +([0-9]+)\].*$" )
-  commits = getCommits( defaultAll = False )
+  refs = getRefs( defaultCurrent = True )
   found = {}
   for line in do( "git branch -v" ).split( "\n" ):
     match = remlog.match( line )
     if match:
       groups = match.groups()
-      if groups[0] in commits:
+      if groups[0] in refs:
 	found[ groups[0] ] = ( groups[1], int( groups[2] ) )
-  for commit in commits:
-    if commit not in found:
+  for ref in refs:
+    if ref not in found:
       continue
-    trackedBranch = getBranchTacks( commit )
-    if found[ commit ][0] == 'ahead':
-      print "\033[;35m*\033[0m %s is \033[;35mAHEAD\033[0m of %s by %d commits:\n" % ( commit, trackedBranch, found[ commit ][1] )
-      sTo = ( getBranchTacks( commit ), commit )
+    trackedBranch = getBranchTacks( ref )
+    if found[ ref ][0] == 'ahead':
+      print "\033[;35m*\033[0m %s is \033[;35mAHEAD\033[0m of %s by %d refs:\n" % ( ref, trackedBranch, found[ ref ][1] )
+      sTo = ( getBranchTacks( ref ), ref )
     else:
-      print "\033[;35m*\033[0m %s is \033[;35mBEHIND\033[0m of %s by %d commits:\n" % ( commit, trackedBranch, found[ commit ][1] )
-      sTo = ( commit, getBranchTacks( commit ) )
+      print "\033[;35m*\033[0m %s is \033[;35mBEHIND\033[0m of %s by %d refs:\n" % ( ref, trackedBranch, found[ ref ][1] )
+      sTo = ( ref, getBranchTacks( ref ) )
     print "%s\n" % do( "git log --color '--pretty=format:%%Cgreen%%H %%Cblue%%d%%Creset%%n%%B' %s..%s" % sTo )
 
 
+    
+def execPrune():
+  if not parseRes.shy:
+    execUpdate()
+  refsToDelete = getRefs()
+  remoteBranches = []
+  for branch in do( "git branch -r" ).split( "\n" ):
+    branch = branch.strip()
+    if branch.find( "origin/HEAD" ) == -1:
+      remoteBranches.append( branch )
+  auto = False
+  if refsToDelete:
+    for ref in refsToDelete:
+      if ref not in remoteBranches:
+	print "%s is not a remote branch" % ref
+	sys.exit(1)
+  else:
+    auto = True
+    for branch in remoteBranches:
+      if branch.find( "origin/" ) == 0:
+	refsToDelete.append( branch )
+  if auto:
+    iP = 0
+    while iP < len( refsToDelete ):
+      branch = refsToDelete[iP].split( "/" )[1]
+      if exists( branch ):
+	refsToDelete.pop( iP )
+      elif raw_input( "%s does not exist locally. Prune? [Y/n]> " % branch ).lower() not in ( 'y', 'yes' ):
+	refsToDelete.pop( iP )
+      else:
+	iP += 1
+  if not refsToDelete:
+    print "No remote branches found to be pruned"
+  else:
+    if raw_input( "About to prune %s. Is it OK? [Y/n]> " % ", ".join( refsToDelete ) ).lower() not in ( 'y', 'yes' ):
+      print "Prune aborted"
+    else:
+      for ref in refsToDelete:
+	print "Pruning %s" % ref
+	do( "git push %s :%s" % tuple( ref.split( "/" ) ) )
+
+  
+#Glue code
+  
+if parseRes.list:
+  doneStuff = True
+  execList()
+if parseRes.update:
+  doneStuff = True
+  execUpdate()
+if parseRes.divergence:
+  doneStuff = True
+  execDivergence()
+if parseRes.aheadlog:
+  doneStuff = True
+  execAheadLog() 
+if parseRes.prune:
+  doneStuff = True
+  execPrune()
+    
 if not doneStuff:
   parser.print_help()
   sys.exit( 1 )
